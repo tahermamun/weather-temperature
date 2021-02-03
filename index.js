968eac410078a9a8c82b5af7197563b21bdf815b
Taher Mamun <tahermamun5@gmail.com>
2021-02-04 02:48:56 +0600

    update

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 document.getElementById('search-btn').addEventListener('click', function () {
 
-
     const locationSearch = document.getElementById('location-input').value;
     const locationAddress = document.getElementById('location-address');
     const temperature = document.getElementById('temperature');
@@ -12,9 +11,11 @@ document.getElementById('search-btn').addEventListener('click', function () {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + locationSearch + '&appid=0493e236ca29d54b8bedbdb3b099e8f5')
         .then(res => res.json())
         .then(data =>{
-            locationAddress.innerText = data.name
-            temperature.innerText = data.main.pressure
-            weatherDescription.innerText = data.weather[0].main
+            console.log(data)
+            locationAddress.innerText = data.name //Location Name Print
+            let temperatureCelsius = Math.round(data.main.temp -273.15) //Temperature Convert Kelvin to Celsius
+            temperature.innerText = temperatureCelsius //Temperature Print
+            weatherDescription.innerText = data.weather[0].main //Weather Status Print
 
         })
 })
